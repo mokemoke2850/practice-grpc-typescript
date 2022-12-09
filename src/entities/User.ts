905ef56a904d71faa8b81c0b0f6d3c07caec558d
shoma <koalakoala.xxx@gmail.com>
2022-12-09 21:40:49 +0900

    fixed user

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,28 +1,7 @@
-import {
-  Column,
-  CreateDateColumn,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
-
-export class User {
-  @PrimaryGeneratedColumn()
-  id!: number;
-
-  @Column()
-  name!: number;
-
-  @CreateDateColumn({
-    comment: '作成日時',
-    precision: 0,
-    default: () => 'CURRENT_TIMESTAMP',
-  })
-  createdAt!: string;
-
-  @UpdateDateColumn({
-    comment: '更新日時',
-    precision: 0,
-    default: () => 'CURRENT_TIMESTAMP',
-  })
-  updatedAt!: string;
+export interface User {
+  id: number;
+  email: string;
+  name: string;
+  status?: 'Happy' | 'Sad';
+  phoneNumbers: string[];
 }
