d8921a8bab4bcc91a56ad12c3958b48b68a3a31d
shoma <koalakoala.xxx@gmail.com>
2022-12-09 22:39:19 +0900

    created user endpoint using tsoa

---
+++ src/controllers/usersController.ts
@@ -0,0 +1,35 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Path,
+  Post,
+  Query,
+  Route,
+  SuccessResponse,
+  Tags,
+} from 'tsoa';
+import { User } from 'entities/User';
+import { UserCreationParams, UsersService } from 'services/usersService';
+
+@Route('users')
+@Tags('user')
+export class UsersController extends Controller {
+  @Get('{userId}')
+  public async getUser(
+    @Path() userId: number,
+    @Query() name?: string
+  ): Promise<User> {
+    return new UsersService().get(userId, name);
+  }
+
+  @SuccessResponse('201', 'Created')
+  @Post()
+  public async createUser(
+    @Body() requestBody: UserCreationParams
+  ): Promise<void> {
+    this.setStatus(201);
+    new UsersService().create(requestBody);
+    return;
+  }
+}
---
+++ src/services/usersService.ts
@@ -0,0 +1,23 @@
+import { User } from 'entities/User';
+
+export type UserCreationParams = Pick<User, 'email' | 'name' | 'phoneNumbers'>;
+
+export class UsersService {
+  public get(id: number, name?: string): User {
+    return {
+      id,
+      email: 'jave@doe.com',
+      name: name ?? 'Jane Doe',
+      status: 'Happy',
+      phoneNumbers: [],
+    };
+  }
+
+  public create(UserCreationParams: UserCreationParams): User {
+    return {
+      id: Math.floor(Math.random() * 10000),
+      status: 'Happy',
+      ...UserCreationParams,
+    };
+  }
+}
